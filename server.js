446818cbff50a4a1f449e366abf426a15f3bb360
Molly Smith <molly.ivester@gmail.com>
2023-05-04 16:09:53 -0400

    updating fix database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-dimensional-94920',
-    user : 'mollysmith',
-    password : '',
-    database : 'smart-brain'
+  connectionString : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
