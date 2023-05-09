2a6c6b90b0a9cbed691c87cfee7d471e8fd6a654
Molly Smith <molly.ivester@gmail.com>
2023-05-09 15:16:25 -0400

    fix

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.get('/', (req, res) => { res.send('success') });
+app.get('/', (req, res) => { res.send('successfully loaded') });
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
 
