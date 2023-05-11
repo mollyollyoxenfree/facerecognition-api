6aa34f64bb75cc391e761acd5dd636272927a751
Molly Smith <molly.ivester@gmail.com>
2023-05-11 16:04:04 -0400

    log

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,5 @@
 const handleRegister = (req, res, db, saltRounds, bcrypt) => {
     const { email, name, password } = req.body;
-    console.log(email, name, password);
     if (!email || !name || !password) {
         return res.status(400).json('incorrect form submission');
     }
--- server.js
+++ server.js
@@ -53,7 +53,6 @@ app.get('/', (req, res) => { res.send('successfully loaded') });
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
 
 app.post('/register', (req, res) => { 
-  console.log(req);
   register.handleRegister(req, res, db, saltRounds, bcrypt) });
 //dependencies injection:
 //injecting dependencies needed by handleRegister
