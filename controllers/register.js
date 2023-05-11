ca9be87c8e83d97993cc79bb827f833ceb244678
Molly Smith <molly.ivester@gmail.com>
2023-05-11 16:01:03 -0400

    logging

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,6 @@
 const handleRegister = (req, res, db, saltRounds, bcrypt) => {
-    console.log(req.body);
     const { email, name, password } = req.body;
+    console.log(email, name, password);
     if (!email || !name || !password) {
         return res.status(400).json('incorrect form submission');
     }
