b28d4a280e29b7b9cdc97ec6547c8dc1ebf09e56
Molly Smith <molly.ivester@gmail.com>
2023-05-12 12:44:43 -0400

    notes

--- server.js
+++ server.js
@@ -17,25 +17,6 @@ const db = knex({
   }
 });
 
-// const { Client } = require('pg');
-
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
-
-// client.connect();
-
-// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-//   if (err) throw err;
-//   for (let row of res.rows) {
-//     console.log(JSON.stringify(row));
-//   }
-//   client.end();
-// });
-
 //initialize new express app
 const app = express();
 
@@ -66,9 +47,6 @@ app.post('/imageurl', (req, res) => { image.handleApicall(req, res) });
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
+  console.log(`app is running on port ${process.env.PORT}`);
 }
 app.listen(port);
-
-// app.listen(process.env.PORT || 3000, () => {
-//     console.log(`app is running on port ${process.env.PORT}`);
-// });
